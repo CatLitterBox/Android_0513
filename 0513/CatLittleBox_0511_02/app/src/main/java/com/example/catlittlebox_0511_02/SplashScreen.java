8ad0a652bc879a144feeadd291a38fb1ec205c09
0220jjhh <0220jjhh@daum.net>
2017-05-13 10:44:52 +0900

    0513

---
+++ 0513/CatLittleBox_0511_02/app/src/main/java/com/example/catlittlebox_0511_02/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.catlittlebox_0511_02;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreen.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(2000)
+                .withBackgroundResource(R.drawable.load);
+
+        View view = config.create();
+
+        setContentView(view);
+    }
+}
